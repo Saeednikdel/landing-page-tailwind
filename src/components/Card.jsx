57e7df432c136d9c4e10fd1bfd20a42518bce9d5
Saeed Nikdel <1991saeed@gmail.com>
2022-06-23 14:39:30 +0330

    read more button

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Card({ item }) {
   return (
-    <div className=" mt-20 flex flex-col space-y-2 items-center rounded-3xl bg-white dark:bg-gray-800 drop-shadow">
+    <div className="mt-20 flex flex-col space-y-2 items-center rounded-3xl bg-white dark:bg-gray-800 drop-shadow">
       <img
         className=" h-44 shadow w-44 rounded-3xl -mt-16 object-cover object-center "
         src={item.image}
@@ -13,6 +13,9 @@ function Card({ item }) {
       </h3>
       <p className="text-gray-600 dark:text-gray-300 text-lg p-8">
         {item.text}
+        <a className="hover:cursor-pointer text-red-500 ml-4 hover:text-red-600">
+          read more
+        </a>
       </p>
     </div>
   );
