30e322a9158ec55fead67f41ac2b65109f9a5ecc
Saeed Nikdel <1991saeed@gmail.com>
2022-06-24 22:31:46 +0330

    color

--- src/components/BtnPrimary.jsx
+++ src/components/BtnPrimary.jsx
@@ -4,7 +4,7 @@ const BtnPrimary = ({ children, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-red-700 rounded-full hover:bg-red-800 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+      className="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-red-600 rounded-full hover:bg-red-700 dark:bg-red-500 dark:hover:bg-red-600">
       {children}
     </button>
   );
